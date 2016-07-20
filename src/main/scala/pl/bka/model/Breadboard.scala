788e489aae2c7893aa556afd54b6cc8f1471f3da
bartlomiej.kalinka <bartlomiej.kalinka@javeo.eu>
2016-07-20 12:32:04 +0200

    logical vs physical board tracks

--- src/main/scala/pl/bka/model/Breadboard.scala
+++ src/main/scala/pl/bka/model/Breadboard.scala
@@ -1,23 +1,22 @@
 package pl.bka.model
 
-sealed trait BreadboardTrack {
-  val length: Int
-}
+sealed trait BreadboardTrack
+case class LogicalTrack(index: Int) extends BreadboardTrack
 case class Vertical(upper: Boolean, index: Int, length: Int) extends BreadboardTrack
-case class Horizontal(upper: Boolean, left: Boolean, index: Boolean, length: Int) extends BreadboardTrack
+case class Horizontal(upper: Boolean, left: Boolean, index: Int, length: Int) extends BreadboardTrack
 
 case class Hole(track: BreadboardTrack, index: Int)
 
 case class Breadboard(
-                     logical: Map[LegId, BreadboardTrack],
+                     logical: Map[LegId, LogicalTrack],
                      physical: Map[LegId, Hole]
                      )
 
 
 object Breadboard {
   def fromDiagram(diagram: Diagram): Breadboard = {
-    val connsTracks: Map[Connection, BreadboardTrack] = diagram.connections.map(conn => (conn, Vertical(true, conn.id, 5))).toMap
-    val logical: Map[LegId, BreadboardTrack] = diagram.connectionsLegs.flatMap {
+    val connsTracks: Map[Connection, LogicalTrack] = diagram.connections.map(conn => (conn, LogicalTrack(conn.id))).toMap
+    val logical: Map[LegId, LogicalTrack] = diagram.connectionsLegs.flatMap {
       case (conn, legs) => legs.map((_, connsTracks(conn)))
     }
     Breadboard(logical, Map.empty[LegId, Hole])
