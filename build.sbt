cea77dce95b9be8f8f5d7b0b973d22217e9d96ee
bartlomiej.kalinka <bartlomiej.kalinka@javeo.eu>
2016-07-21 13:44:28 +0200

    real output to html

--- build.sbt
+++ build.sbt
@@ -6,4 +6,6 @@ scalaVersion := "2.11.8"
 
 scalaJSUseRhino in Global := false
 
+libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.9.0"
+
 enablePlugins(ScalaJSPlugin)
--- src/main/scala/pl/bka/Main.scala
+++ src/main/scala/pl/bka/Main.scala
@@ -1,15 +1,25 @@
 package pl.bka
 
 import scala.scalajs.js.JSApp
+import org.scalajs.dom
+import dom.document
 import pl.bka.model.{Diagram, Breadboard}
 
 object Main extends JSApp {
+
+  def domPrintln(text: String) = {
+    val textNode = document.createTextNode(text)
+    val parNode = document.createElement("p")
+    parNode.appendChild(textNode)
+    document.body.appendChild(parNode)
+  }
+
   def main(): Unit = {
-    println(s"avtDog: ${Diagram.prettyPrint(Diagrams.example)}")
+    domPrintln(s"avtDog: ${Diagram.prettyPrint(Diagrams.example)}")
     Diagrams.example match {
       case Right(diagram) =>
         val exampleBoard = Breadboard.fromDiagram(diagram)
-        println(s"exampleBoard: ${exampleBoard.prettyPrint}")
+        domPrintln(s"exampleBoard: ${exampleBoard.prettyPrint}")
       case _ => ()
     }
   }
