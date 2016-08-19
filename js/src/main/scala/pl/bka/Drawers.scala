323dc20bbba5bf29eabc40a08d64b320e4da3265
Bartek Kalinka <bartkali@hotmail.com>
2016-08-19 23:27:57 +0200

    simplified drawing + prepare to draw components

--- js/src/main/scala/pl/bka/Drawers.scala
+++ js/src/main/scala/pl/bka/Drawers.scala
@@ -1,11 +1,7 @@
 package pl.bka
 
-import pl.bka.model.Diagram
-import pl.bka.model.breadboard.{Physical, Track, Vertical}
-
-trait Drawer {
-  def draw(position: Int): Unit
-}
+import pl.bka.model.{Component, Diagram, LegId}
+import pl.bka.model.breadboard.{Hole, Physical, Track, Vertical}
 
 object Drawers {
   val verticalTracksStep = 15
@@ -13,28 +9,29 @@ object Drawers {
   val verticalTracksHorizontalOffset = 2 * verticalTracksStep
   val verticalTracksVerticalOffset = 10
 
-  implicit def verticalDrawer(vertical: Vertical): Drawer = new Drawer {
-    def draw(offset: Int): Unit = {
-      val ctx = DomOutput.renderer
-      ctx.strokeStyle = "#000000"
-      ctx.lineWidth = 2
-      ctx.beginPath()
-      ctx.moveTo(vertical.index.index * verticalTracksStep + verticalTracksHorizontalOffset, offset)
-      ctx.lineTo(vertical.index.index * verticalTracksStep + verticalTracksHorizontalOffset, offset + verticalTrackLength)
-      ctx.stroke()
-    }
+  def drawVerticalTrack(vertical: Vertical, offset: Int): Unit = {
+    val ctx = DomOutput.renderer
+    ctx.strokeStyle = "#000000"
+    ctx.lineWidth = 2
+    ctx.beginPath()
+    ctx.moveTo(vertical.index.index * verticalTracksStep + verticalTracksHorizontalOffset, offset)
+    ctx.lineTo(vertical.index.index * verticalTracksStep + verticalTracksHorizontalOffset, offset + verticalTrackLength)
+    ctx.stroke()
   }
 
-  implicit def trackDrawer(track: Track): Drawer = new Drawer {
-    def draw(offset: Int): Unit = track match {
-      case v: Vertical => v.draw(offset)
+  def drawPhysical(physical: Physical, diagram: Diagram, offset: Int): Unit = {
+    def drawTrack(track: Track, offset: Int): Unit = track match {
+      case v: Vertical => drawVerticalTrack(v, offset)
       case _ => ()
     }
-  }
-
-  def physicalWithDiagramDrawer(physical: Physical, diagram: Diagram): Drawer = new Drawer {
-    def draw(offset: Int): Unit = {
-      physical.tracks.foreach(_.draw(verticalTracksVerticalOffset))
+    def drawComponent(component: Component, offset: Int): Unit = {
+      val holes: Seq[Hole] = component.legs.map { leg =>
+        physical.connections(LegId(component.name, leg))
+      }
+      val trackIndices = holes.map(_.trackIndex.index)
+      val middle = trackIndices.sum / trackIndices.length
     }
+    physical.tracks.foreach(drawTrack(_, verticalTracksVerticalOffset))
+    diagram.components.foreach(drawComponent(_, offset))
   }
 }
