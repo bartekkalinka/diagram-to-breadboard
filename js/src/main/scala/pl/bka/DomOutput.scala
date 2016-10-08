4f1c6fd885f9a61e26938a4db06fb15bc3f9ec62
Bartek Kalinka <bartkali@hotmail.com>
2016-10-08 13:58:56 +0200

    ctx -> drawers

--- js/src/main/scala/pl/bka/DomOutput.scala
+++ js/src/main/scala/pl/bka/DomOutput.scala
@@ -1,13 +1,9 @@
 package pl.bka
 
-import org.scalajs.dom
 import org.scalajs.dom.{html, _}
-import pl.bka.model.breadboard.{Physical, Track, Vertical}
 
 object DomOutput {
   val canvas = document.getElementById("canvas").asInstanceOf[html.Canvas]
-  val renderer = canvas.getContext("2d")
-    .asInstanceOf[dom.CanvasRenderingContext2D]
 
   def println(text: String) = {
     val textNode = document.createTextNode(text)
--- js/src/main/scala/pl/bka/Drawers.scala
+++ js/src/main/scala/pl/bka/Drawers.scala
@@ -1,13 +1,17 @@
 package pl.bka
 
+import org.scalajs.dom
 import pl.bka.model.{Component, Diagram, LegId}
-import pl.bka.model.breadboard.{Hole, Physical, Track, Vertical}
+import pl.bka.model.breadboard._
 
 object Drawers {
   val verticalTracksStep = 15
-  val verticalTrackLength = 125
+  val verticalTrackLength = 25 * Tracks.verticalTrackLength
   val verticalTracksHorizontalOffset = 2 * verticalTracksStep
   val verticalTracksVerticalOffset = 10
+
+  val ctx = DomOutput.canvas.getContext("2d")
+    .asInstanceOf[dom.CanvasRenderingContext2D]
 
   def drawPhysical(physical: Physical, diagram: Diagram): Unit = {
     def drawComponent(component: Component): Unit = {
@@ -22,7 +26,6 @@ object Drawers {
   }
 
   private def drawVerticalTrack(vertical: Vertical): Unit = {
-    val ctx = DomOutput.renderer
     ctx.strokeStyle = "#000000"
     ctx.lineWidth = 2
     ctx.beginPath()
