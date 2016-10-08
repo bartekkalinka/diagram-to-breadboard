a453dea09b7b381f7e7b511af4c4a8a45737e3c4
Bartek Kalinka <bartkali@hotmail.com>
2016-10-08 13:40:00 +0200

    drawers private methods

--- js/src/main/scala/pl/bka/Drawers.scala
+++ js/src/main/scala/pl/bka/Drawers.scala
@@ -9,21 +9,7 @@ object Drawers {
   val verticalTracksHorizontalOffset = 2 * verticalTracksStep
   val verticalTracksVerticalOffset = 10
 
-  def drawVerticalTrack(vertical: Vertical): Unit = {
-    val ctx = DomOutput.renderer
-    ctx.strokeStyle = "#000000"
-    ctx.lineWidth = 2
-    ctx.beginPath()
-    ctx.moveTo(vertical.index.index * verticalTracksStep + verticalTracksHorizontalOffset, verticalTracksVerticalOffset)
-    ctx.lineTo(vertical.index.index * verticalTracksStep + verticalTracksHorizontalOffset, verticalTracksVerticalOffset + verticalTrackLength)
-    ctx.stroke()
-  }
-
   def drawPhysical(physical: Physical, diagram: Diagram): Unit = {
-    def drawTrack(track: Track, offset: Int): Unit = track match {
-      case v: Vertical => drawVerticalTrack(v)
-      case _ => ()
-    }
     def drawComponent(component: Component): Unit = {
       val holes: Seq[Hole] = component.legs.map { leg =>
         physical.connections(LegId(component.name, leg))
@@ -33,5 +19,20 @@ object Drawers {
     }
     physical.tracks.foreach(drawTrack(_, verticalTracksVerticalOffset))
     diagram.components.foreach(drawComponent)
+  }
+
+  private def drawVerticalTrack(vertical: Vertical): Unit = {
+    val ctx = DomOutput.renderer
+    ctx.strokeStyle = "#000000"
+    ctx.lineWidth = 2
+    ctx.beginPath()
+    ctx.moveTo(vertical.index.index * verticalTracksStep + verticalTracksHorizontalOffset, verticalTracksVerticalOffset)
+    ctx.lineTo(vertical.index.index * verticalTracksStep + verticalTracksHorizontalOffset, verticalTracksVerticalOffset + verticalTrackLength)
+    ctx.stroke()
+  }
+
+  private def drawTrack(track: Track, offset: Int): Unit = track match {
+    case v: Vertical => drawVerticalTrack(v)
+    case _ => ()
   }
 }
