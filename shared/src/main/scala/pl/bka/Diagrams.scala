ea0058227011c87e5a4be596c96dda4d24357475
Bartek Kalinka <bartkali@hotmail.com>
2018-01-06 22:16:28 +0100

    correct connection index in example diagram

--- shared/src/main/scala/pl/bka/Diagrams.scala
+++ shared/src/main/scala/pl/bka/Diagrams.scala
@@ -15,13 +15,13 @@ object Diagrams {
       Component("R220-5", Resistor("220K")),
       Component("R220-6", Resistor("220K"))
     ),
-    Map(("Tr549B.1", "0") -> Right(Plus), ("Tr549B.1", "1") -> Left(1), ("Tr549B.1", "2") -> Right(GND),
-      ("Tr549B.2", "0") -> Right(Plus), ("Tr549B.2", "1") -> Left(1), ("Tr549B.2", "2") -> Right(GND),
-      ("R220-1", "0") -> Right(GND), ("R220-1", "1") -> Left(1),
-      ("R220-2", "0") -> Right(Plus), ("R220-2", "1") -> Left(1),
-      ("R220-3", "0") -> Left(1), ("R220-3", "1") -> Right(GND),
-      ("R220-4", "0") -> Right(GND), ("R220-4", "1") -> Left(1),
-      ("R220-5", "0") -> Left(1), ("R220-5", "1") -> Right(Plus),
-      ("R220-6", "0") -> Right(GND), ("R220-6", "1") -> Left(1))
+    Map(("Tr549B.1", "0") -> Right(Plus), ("Tr549B.1", "1") -> Left(0), ("Tr549B.1", "2") -> Right(GND),
+      ("Tr549B.2", "0") -> Right(Plus), ("Tr549B.2", "1") -> Left(0), ("Tr549B.2", "2") -> Right(GND),
+      ("R220-1", "0") -> Right(GND), ("R220-1", "1") -> Left(0),
+      ("R220-2", "0") -> Right(Plus), ("R220-2", "1") -> Left(0),
+      ("R220-3", "0") -> Left(0), ("R220-3", "1") -> Right(GND),
+      ("R220-4", "0") -> Right(GND), ("R220-4", "1") -> Left(0),
+      ("R220-5", "0") -> Left(0), ("R220-5", "1") -> Right(Plus),
+      ("R220-6", "0") -> Right(GND), ("R220-6", "1") -> Left(0))
   )
 }
