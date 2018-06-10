3c3e7eaa3beea1efb9ec9b00d30abdf9e5fe73e0
Kalinka <bartkali@hotmail.com>
2018-06-10 20:33:47 +0200

    rollz-5 3-roll diagram

--- shared/src/main/scala/pl/bka/Diagrams.scala
+++ shared/src/main/scala/pl/bka/Diagrams.scala
@@ -6,6 +6,41 @@ import pl.bka.model.Power._
 object Diagrams {
   val example = Diagram(
     List(
+      Component("diode", Diode("???")),
+      Component("R470K-1", Resistor("470K")),
+      Component("R22K-1", Resistor("22K")),
+      Component("R470K-2", Resistor("470K")),
+      Component("R22K-2", Resistor("22K")),
+      Component("R470K-3", Resistor("470K")),
+      Component("R22K-3", Resistor("22K")),
+      Component("Tr-1", Transistor("minus")),
+      Component("Tr-2", Transistor("minus")),
+      Component("Tr-3", Transistor("minus")),
+      Component("hairy-1", Capacitor(0d, bipolar = true)),
+      Component("hairy-2", Capacitor(0d, bipolar = true)),
+      Component("hairy-3", Capacitor(0d, bipolar = true)),
+      Component("cap-4", Capacitor(0d, bipolar = true))
+    ),
+    Map(
+      ("diode", Leg.cathode) -> Left(1), ("diode", Leg.anode) -> Right(Plus),
+      ("R470K-1", "0") -> Left(1), ("R470K-1", "1") -> Left(3),
+      ("R22K-1", "0") -> Left(1), ("R22K-1", "1") -> Left(2),
+      ("R470K-2", "0") -> Left(1), ("R470K-2", "1") -> Left(7),
+      ("R22K-2", "0") -> Left(1), ("R22K-2", "1") -> Left(6),
+      ("R470K-3", "0") -> Left(1), ("R470K-3", "1") -> Left(5),
+      ("R22K-3", "0") -> Left(1), ("R470K-3", "1") -> Left(4),
+      ("Tr-1", "0") -> Left(2), ("Tr-1", "1") -> Left(3), ("Tr-1", "2") -> Right(GND),
+      ("Tr-2", "0") -> Left(6), ("Tr-2", "1") -> Left(7), ("Tr-2", "2") -> Right(GND),
+      ("Tr-3", "0") -> Left(4), ("Tr-3", "1") -> Left(5), ("Tr-3", "2") -> Right(GND),
+      ("hairy-1", Leg.capMinus) -> Left(7), ("hairy-1", Leg.capPlus) -> Left(2),
+      ("hairy-2", Leg.capMinus) -> Left(3), ("hairy-2", Leg.capPlus) -> Left(4),
+      ("hairy-3", Leg.capMinus) -> Left(5), ("hairy-3", Leg.capPlus) -> Left(6),
+      ("cap-4", Leg.capMinus) -> Right(GND), ("cap-4", Leg.capPlus) -> Left(1)
+    )
+  )
+
+  val example2 = Diagram(
+    List(
       Component("Tr549B.1", Transistor("549B")),
       Component("Tr549B.2", Transistor("549B")),
       Component("R220-1", Resistor("220K")),
