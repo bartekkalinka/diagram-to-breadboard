1ce25531e3a892a333999818f5db060711e99edb
Kalinka <bartkali@hotmail.com>
2018-06-17 20:35:03 +0200

    js main on window load + canvas onmousemove sample

--- foo/js/src/main/scala/pl/bka/Main.scala
+++ foo/js/src/main/scala/pl/bka/Main.scala
@@ -1,17 +1,18 @@
 package pl.bka
 
 import org.scalajs.dom
+import org.scalajs.dom.{document, html, window}
 import pl.bka.drawing.{BoardDrawing, Size}
 import pl.bka.model.Diagram
 import pl.bka.model.breadboard.Breadboard
 
-import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
+import scala.scalajs.js.annotation.JSExportTopLevel
 
 @JSExportTopLevel("Main")
 object Main {
-
-  @JSExport
-  def main(): Unit = {
+  window.onload = { _ =>
+    val offsetX = DomOutput.canvas.offsetLeft
+    val offsetY = DomOutput.canvas.offsetTop
     DomOutput.println("diagram")
     Diagram.prettyPrint(Diagrams.example).foreach(DomOutput.println)
     Diagrams.example match {
@@ -23,6 +24,11 @@ object Main {
         val boardDrawing = new BoardDrawing(new Size(2d))
         boardDrawing.drawPhysical(exampleBoard.physical, diagram)
       case _ => ()
+    }
+    val coordDiv = document.getElementById("coord").asInstanceOf[html.Div]
+    DomOutput.canvas.onmousemove = { e =>
+      val coord = s"${e.clientX - offsetX}, ${e.clientY - offsetY}"
+      coordDiv.innerHTML = coord
     }
   }
 }
