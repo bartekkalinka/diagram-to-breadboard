5c50ecbf1110a977c24f53143162e0d2fa6880b7
Kalinka <bartkali@hotmail.com>
2019-02-10 09:22:39 +0100

    start index for tracks labels

--- foo/js/src/main/scala/pl/bka/drawing/DrawingLayer.scala
+++ foo/js/src/main/scala/pl/bka/drawing/DrawingLayer.scala
@@ -4,17 +4,18 @@ import pl.bka.model.breadboard.{Breadboard, TrackIndex}
 
 object DrawingLayer {
   val isLowerBoardNumberingReversed = false
+  val startIndex = 1
 
   implicit class TrackIndexLabeling(index: TrackIndex) {
     def label: String =
       if(index.upper) {
-        "U" + "%02d".format(index.index)
+        "U" + "%02d".format(index.index + startIndex)
       } else {
         val lowerIndex =
           if(isLowerBoardNumberingReversed) {
             -index.index
           } else {
-            Breadboard.maxVerticalTracks + index.index + 1
+            Breadboard.maxVerticalTracks + index.index + startIndex
           }
         "D" + "%02d".format(lowerIndex)
       }
