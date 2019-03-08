cbb82cbbe55920669dd8fe0e097f1f2c6fd00a87
Kalinka <bartkali@hotmail.com>
2019-03-08 20:13:14 +0100

    parser: all 2-leg components

--- dtb/jvm/src/test/scala/pl/bka/dtb/DiagramParserSpec.scala
+++ dtb/jvm/src/test/scala/pl/bka/dtb/DiagramParserSpec.scala
@@ -1,11 +1,13 @@
 package pl.bka.dtb
 
 import org.scalatest.{FlatSpec, Matchers}
-import pl.bka.dtb.model.{Diode, Transistor}
+import pl.bka.dtb.model.{Capacitor, Diode, Resistor, Transistor}
 
 class DiagramParserSpec extends FlatSpec with Matchers {
   "DiagramParser" should "parse line" in {
     DiagramLineEncodingParser.parseDiagram("t 3 5 1") shouldBe Right(Transistor(""), Seq(3, 5, 1))
     DiagramLineEncodingParser.parseDiagram("d 3 5") shouldBe Right(Diode(""), Seq(3, 5))
+    DiagramLineEncodingParser.parseDiagram("r 8 5") shouldBe Right(Resistor(""), Seq(8, 5))
+    DiagramLineEncodingParser.parseDiagram("c 8 5") shouldBe Right(Capacitor(0d, bipolar = true), Seq(8, 5))
   }
 }
--- dtb/shared/src/main/scala/pl/bka/dtb/DiagramParser.scala
+++ dtb/shared/src/main/scala/pl/bka/dtb/DiagramParser.scala
@@ -13,17 +13,19 @@ object DiagramLineEncodingParser extends RegexParsers {
   type Result = (ComponentType, Seq[Int])
 
   private def diode: Parser[Diode] = "d" ^^ { _ => Diode("") }
+  private def resistor: Parser[Resistor] = "r" ^^ { _ => Resistor("") }
+  private def capacitor: Parser[Capacitor] = "c" ^^ { _ => Capacitor(0d, bipolar = true) } //TODO better params
   private def transistor: Parser[Transistor] = "t" ^^ { _ => Transistor("") }
 
   private def legId: Parser[Int] = """[1-9]|[1-9]\\d*""".r ^^ { _.toInt }
 
-  private def diodeLine: Parser[Result] =
-    diode ~ legId ~ legId ^^ { case d ~ lid1 ~ lid2 => (d, List(lid1, lid2)) }
+  private def twoLegsLine: Parser[Result] =
+    (diode | resistor | capacitor) ~ legId ~ legId ^^ { case ct ~ lid1 ~ lid2 => (ct, List(lid1, lid2)) }
 
   private def transistorLine: Parser[Result] =
     transistor ~ legId ~ legId ~ legId ^^ { case t ~ lid1 ~ lid2 ~ lid3 => (t, List(lid1, lid2, lid3)) }
 
-  private def line: Parser[Result] = diodeLine | transistorLine
+  private def line: Parser[Result] = twoLegsLine | transistorLine
 
   def parseDiagram(diagramStr: String): Either[String, Result] =
     parse(line, diagramStr) match {
