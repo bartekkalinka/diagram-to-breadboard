15cd2c6d6cdbc28600a1c7cef9640c2fab0de312
Bartek <bartkali@hotmail.com>
2019-05-11 20:35:03 +0200

    porting diagrams to new format pt. 5

--- dtb/jvm/src/test/scala/pl/bka/dtb/DiagramParserSpec.scala
+++ dtb/jvm/src/test/scala/pl/bka/dtb/DiagramParserSpec.scala
@@ -143,4 +143,59 @@ class DiagramParserSpec extends FlatSpec with Matchers {
       )
     ))
   }
+
+  it should "parse 3-roll diagram" in {
+    DiagramParser.parse(
+      """
+        |d.diode band.1 plus
+        |r.R470K-1 1 3
+        |r.22K-1 1 2
+        |r.R470K-2 1 7
+        |r.22K-2 1 6
+        |r.R470K-3 1 5
+        |r.22K-3 1 4
+        |t.Tr-1 2 3 gnd
+        |t.Tr-2 6 7 gnd
+        |t.Tr-3 4 5 gnd
+        |bc.hairy-1 -7 +2
+        |bc.hairy-2 -3 +4
+        |bc.hairy-3 -5 +6
+        |bc.cap-4 -gnd +1
+      """.stripMargin
+    )
+    Right((
+      List(
+        Component("diode", Diode()),
+        Component("R470K-1", Resistor()),
+        Component("R22K-1", Resistor()),
+        Component("R470K-2", Resistor()),
+        Component("R22K-2", Resistor()),
+        Component("R470K-3", Resistor()),
+        Component("R22K-3", Resistor()),
+        Component("Tr-1", Transistor()),
+        Component("Tr-2", Transistor()),
+        Component("Tr-3", Transistor()),
+        Component("hairy-1", Capacitor(bipolar = true)),
+        Component("hairy-2", Capacitor(bipolar = true)),
+        Component("hairy-3", Capacitor(bipolar = true)),
+        Component("cap-4", Capacitor(bipolar = true))
+      ),
+      Map(
+        ("diode", Leg.cathode) -> Left(1), ("diode", Leg.anode) -> Right(Plus),
+        ("R470K-1", Leg.firstLeg) -> Left(1), ("R470K-1", Leg.secondLeg) -> Left(3),
+        ("R22K-1", Leg.firstLeg) -> Left(1), ("R22K-1", Leg.secondLeg) -> Left(2),
+        ("R470K-2", Leg.firstLeg) -> Left(1), ("R470K-2", Leg.secondLeg) -> Left(7),
+        ("R22K-2", Leg.firstLeg) -> Left(1), ("R22K-2", Leg.secondLeg) -> Left(6),
+        ("R470K-3", Leg.firstLeg) -> Left(1), ("R470K-3", Leg.secondLeg) -> Left(5),
+        ("R22K-3", Leg.firstLeg) -> Left(1), ("R22K-3", Leg.secondLeg) -> Left(4),
+        ("Tr-1", "0") -> Left(2), ("Tr-1", "1") -> Left(3), ("Tr-1", "2") -> Right(GND),
+        ("Tr-2", "0") -> Left(6), ("Tr-2", "1") -> Left(7), ("Tr-2", "2") -> Right(GND),
+        ("Tr-3", "0") -> Left(4), ("Tr-3", "1") -> Left(5), ("Tr-3", "2") -> Right(GND),
+        ("hairy-1", Leg.capMinus) -> Left(7), ("hairy-1", Leg.capPlus) -> Left(2),
+        ("hairy-2", Leg.capMinus) -> Left(3), ("hairy-2", Leg.capPlus) -> Left(4),
+        ("hairy-3", Leg.capMinus) -> Left(5), ("hairy-3", Leg.capPlus) -> Left(6),
+        ("cap-4", Leg.capMinus) -> Right(GND), ("cap-4", Leg.capPlus) -> Left(1)
+      )
+    ))
+  }
 }
