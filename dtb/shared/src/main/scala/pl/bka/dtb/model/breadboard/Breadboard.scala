358f2d5069681cd39a6389e3e5f168aafdf5e37e
Bartek <bartkali@hotmail.com>
2019-09-18 18:21:49 +0200

    corrections for avdog

--- dtb/shared/src/main/scala/pl/bka/dtb/model/breadboard/Breadboard.scala
+++ dtb/shared/src/main/scala/pl/bka/dtb/model/breadboard/Breadboard.scala
@@ -5,7 +5,7 @@ import pl.bka.dtb.model._
 case class Breadboard(logical: Logical, physical: Physical)
 
 object Breadboard {
-  val maxVerticalTracks = 62
+  val maxVerticalTracks = 5000
   def sideStartIndex(upper: Boolean): Int = if(upper) 0 else -Breadboard.maxVerticalTracks
 
   def apply(diagram: Diagram): Breadboard = {
